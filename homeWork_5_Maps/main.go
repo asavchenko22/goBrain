3ac71af24756dda542a7172e7d7dc21780c8b183
Andrey <saowwll@gmail.com>
2021-12-17 17:52:49 +0200

    completed hw

--- homeWork_5_Maps/main.go
+++ homeWork_5_Maps/main.go
@@ -4,6 +4,29 @@ import (
 	"fmt"
 )
 
-func main () {
-	fmt.Println("oh")
+func main() {
+	issuedEdition := map[string]map[string][]string{
+		"aSavchenko": {
+			"Java.Professional library": {"Volume 1. Basics.", "Volume 2. Advanced Software"},
+		},
+		"rStokolyas": {
+			"Cloud Go": {"first edition"},
+		},
+		"dShvets": {
+			"Ruby": {"Ruby for Romantics. From beginner to professional", "Ruby for Romantics - Second Edition"},
+		},
+	}
+
+	numberOfReaders := len(issuedEdition)
+
+	fmt.Printf("number of readers : %v\n", numberOfReaders)
+
+	for k, v := range issuedEdition {
+		fmt.Print(k, ": ")
+		for subK, subV := range v {
+			fmt.Print(subK, ": ", len(subV), " edition")
+		}
+		fmt.Println()
+	}
+
 }
